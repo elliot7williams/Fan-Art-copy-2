72b5a270a259021337137b23f16007865c17b253
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:00:18 -0400

    Add Fan Art copy 2/Fan ArtTests/Fan_ArtTests.swift

---
+++ Fan Art copy 2/Fan ArtTests/Fan_ArtTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fan_ArtTests.swift
+//  Fan ArtTests
+//
+//  Created by Elliot Williams on 2025-07-04.
+//
+
+import Testing
+@testable import Fan_Art
+
+struct Fan_ArtTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
